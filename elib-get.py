1c03de695b2724bc96549884cab7a42ee9d5c2b0
Jacques de Laval <chucky@wrutschkow.org>
2011-11-27 15:12:16 +0100

    Spelling

--- elib-get.py
+++ elib-get.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 # get-elib.py
 #      
-# Copyright 2011 Jacques de Lval chucky@wrutschkow.org
+# Copyright 2011 Jacques de Laval chucky@wrutschkow.org
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
